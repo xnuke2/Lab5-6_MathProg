553ee09ffd7157aaa05b470b76bccc2bada88b24
xnuke2 <xmnikit@gmail.com>
2025-03-26 04:38:02 +0300

    Добавьте файлы проекта.

---
+++ Lab5-6_MathProg/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Lab5_6_MathProg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewConditions = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonCulculate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridViewSolutions = new System.Windows.Forms.DataGridView();
+            this.dataGridViewResult = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewConditions)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSolutions)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewConditions
+            // 
+            this.dataGridViewConditions.AllowUserToAddRows = false;
+            this.dataGridViewConditions.AllowUserToDeleteRows = false;
+            this.dataGridViewConditions.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewConditions.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridViewConditions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewConditions.ColumnHeadersVisible = false;
+            this.dataGridViewConditions.Location = new System.Drawing.Point(6, 40);
+            this.dataGridViewConditions.Name = "dataGridViewConditions";
+            this.dataGridViewConditions.RowHeadersVisible = false;
+            this.dataGridViewConditions.Size = new System.Drawing.Size(381, 188);
+            this.dataGridViewConditions.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(127, 14);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(41, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(333, 14);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(39, 20);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество строк";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Количество столбцов";
+            // 
+            // buttonCulculate
+            // 
+            this.buttonCulculate.Location = new System.Drawing.Point(269, 234);
+            this.buttonCulculate.Name = "buttonCulculate";
+            this.buttonCulculate.Size = new System.Drawing.Size(118, 22);
+            this.buttonCulculate.TabIndex = 5;
+            this.buttonCulculate.Text = "Вычислить";
+            this.buttonCulculate.UseVisualStyleBackColor = true;
+            this.buttonCulculate.Click += new System.EventHandler(this.buttonCulculate_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridViewConditions);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.buttonCulculate);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(393, 264);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Условия";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridViewResult);
+            this.groupBox2.Controls.Add(this.dataGridViewSolutions);
+            this.groupBox2.Location = new System.Drawing.Point(12, 282);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(393, 277);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Решение";
+            // 
+            // dataGridViewSolutions
+            // 
+            this.dataGridViewSolutions.AllowUserToAddRows = false;
+            this.dataGridViewSolutions.AllowUserToDeleteRows = false;
+            this.dataGridViewSolutions.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewSolutions.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridViewSolutions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSolutions.ColumnHeadersVisible = false;
+            this.dataGridViewSolutions.Location = new System.Drawing.Point(6, 19);
+            this.dataGridViewSolutions.Name = "dataGridViewSolutions";
+            this.dataGridViewSolutions.RowHeadersVisible = false;
+            this.dataGridViewSolutions.Size = new System.Drawing.Size(381, 188);
+            this.dataGridViewSolutions.TabIndex = 6;
+            // 
+            // dataGridViewResult
+            // 
+            this.dataGridViewResult.AllowUserToAddRows = false;
+            this.dataGridViewResult.AllowUserToDeleteRows = false;
+            this.dataGridViewResult.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewResult.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridViewResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResult.Location = new System.Drawing.Point(6, 213);
+            this.dataGridViewResult.Name = "dataGridViewResult";
+            this.dataGridViewResult.RowHeadersVisible = false;
+            this.dataGridViewResult.Size = new System.Drawing.Size(381, 53);
+            this.dataGridViewResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 610);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewConditions)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSolutions)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewConditions;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonCulculate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridViewSolutions;
+        private System.Windows.Forms.DataGridView dataGridViewResult;
+    }
+}
+
---
+++ Lab5-6_MathProg/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5_6_MathProg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dataGridViewConditions.Rows.Clear();
+            dataGridViewConditions.Columns.Clear();
+
+            int rows =Convert.ToInt32(numericUpDown1.Value);
+            int columns =Convert.ToInt32(numericUpDown2.Value);
+
+            for (int i = 0; i < columns + 1; i++)
+                dataGridViewConditions.Columns.Add(""+i, "" + i);
+            for (int i = 0; i < rows + 1; i++)
+                dataGridViewConditions.Rows.Add();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            dataGridViewCellStyle1.BackColor = Color.Cyan;
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            for (int i = 0;i < columns + 1; i++)
+                dataGridViewConditions[i,0].Style = dataGridViewCellStyle1;
+            for(int i = 0;i<rows + 1; i++)
+                dataGridViewConditions[0,i].Style = dataGridViewCellStyle1;
+            for (int i = 0; i < columns + 1; i++)
+                for (int j = 0; j < rows + 1; j++)
+                    dataGridViewConditions[i, j].Value = 0;
+            dataGridViewConditions[0, 0].Value = "";
+            dataGridViewConditions[0, 0].ReadOnly = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numericUpDown1_ValueChanged(sender, e);
+        }
+
+        private void buttonCulculate_Click(object sender, EventArgs e)
+        {
+            int rowsCount = dataGridViewConditions.RowCount-1;
+            int columnsCount = dataGridViewConditions.ColumnCount-1;
+            int[] columns =new int[columnsCount];
+            int[] rows = new int[rowsCount];
+            int[,] costs=new int[rowsCount, columnsCount];
+
+            for(int i = 0;i< rowsCount;i++)
+                rows[i] = Convert.ToInt32(dataGridViewConditions[0,i+1].Value);
+
+            for(int i = 0;i< columnsCount;i++)
+                columns[i] = Convert.ToInt32(dataGridViewConditions[i+1,0].Value);
+
+            for (int i = 0; i< rowsCount;i++)
+                for(int j = 0;j< columnsCount;j++)
+                    costs[i,j] =Convert.ToInt32(dataGridViewConditions[j+1,i+1].Value);
+
+            //При необходимости перевод открытой модели к закрытой
+            if (rows.Sum() != columns.Sum())
+                if (rows.Sum() > columns.Sum())
+                {
+                    int[] tmpColumns = new int[columnsCount + 1];
+                    for (int i = 0; i < columnsCount; i++)
+                        tmpColumns[i] = columns[i];
+                    columns = tmpColumns;
+                    tmpColumns[columnsCount++] = rows.Sum() - columns.Sum();
+                    int[,] tmpCosts = new int[rowsCount, columnsCount];
+                    for (int i = 0;i< rowsCount;i++)
+                        for (int j = 0; j< columnsCount-1;j++)
+                            tmpCosts[i,j]=costs[i,j];
+                    costs = tmpCosts;
+                }
+                else
+                {
+                    int[] tmpRows = new int[rowsCount + 1];
+                    for (int i = 0; i < rowsCount; i++)
+                        tmpRows[i] = rows[i];
+                    rows = tmpRows;
+                    tmpRows[rowsCount++] = columns.Sum()  - rows.Sum();
+                    int[,] tmpCosts = new int[rowsCount, columnsCount];
+                    for (int i = 0; i < rowsCount-1; i++)
+                        for (int j = 0; j < columnsCount; j++)
+                            tmpCosts[i, j] = costs[i, j];
+                    costs = tmpCosts;
+                }
+            int?[,] counts = new int?[rowsCount, columnsCount];
+            int rowIndex = 0;
+            int columnsIndex = 0;
+
+            int[] columnsTmp =new int[columnsCount];
+            columns.CopyTo(columnsTmp, 0);
+            int[] rowsTmp =new int[rowsCount];
+            rows.CopyTo(rowsTmp, 0);
+
+            List<Point> basis = new List<Point>();
+
+            while (rowIndex < rowsCount && columnsIndex < columnsCount)
+            {
+                basis.Add(new Point(rowIndex, columnsIndex));
+                if (columnsTmp[columnsIndex] > rowsTmp[rowIndex])
+                { 
+                    counts[rowIndex, columnsIndex] = rowsTmp[rowIndex];
+                    columnsTmp[columnsIndex] -= rowsTmp[rowIndex];
+                    rowsTmp[rowIndex]=0;
+                    rowIndex++;
+                }
+                else if(columnsTmp[columnsIndex] < rowsTmp[rowIndex])
+                {
+                    counts[rowIndex, columnsIndex] = columnsTmp[columnsIndex];
+                    rowsTmp[rowIndex] -= columnsTmp[columnsIndex];
+                    columnsTmp[columnsIndex] = 0;
+                    columnsIndex++;
+                }  
+                else 
+                {
+                    if (rowIndex == rowsCount - 1 && columnsIndex == columnsCount - 1)
+                    {
+                        counts[rowIndex, columnsIndex] = rowsTmp[rowIndex];
+                        rowsTmp[rowIndex] = 0;
+                        columnsTmp[columnsIndex] = 0;
+                        rowIndex++;
+                    }
+                    else
+                    {
+                        if(rowIndex == rowsCount - 1)
+                        {
+                            counts[rowIndex, columnsIndex] = rowsTmp[rowIndex];
+                            rowsTmp[rowIndex] = 0;
+                            columnsTmp[columnsIndex] = 0;
+                            columnsIndex++;
+                            counts[rowIndex, columnsIndex] = 0;
+                            basis.Add(new Point(rowIndex, columnsIndex));
+                            rowIndex++;
+                        }
+                        else
+                        {
+                            counts[rowIndex, columnsIndex] = rowsTmp[rowIndex];
+                            rowsTmp[rowIndex] = 0;
+                            columnsTmp[columnsIndex] = 0;
+                            rowIndex++;
+                            counts[rowIndex, columnsIndex] = 0;
+                            basis.Add(new Point(rowIndex, columnsIndex));
+                            columnsIndex++;
+                        }
+                    }
+
+                }
+
+            }
+            dataGridViewSolutions.Rows.Clear();
+            dataGridViewSolutions.Columns.Clear();
+            for (int i = 0; i < columns.Length + 1; i++)
+                dataGridViewSolutions.Columns.Add("" + i, "" + i);
+            for (int i = 0; i < rows.Length + 1; i++)
+                dataGridViewSolutions.Rows.Add();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            dataGridViewCellStyle1.BackColor = Color.Cyan;
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            for (int i = 0; i < columns.Length + 1; i++)
+                dataGridViewSolutions[i, 0].Style = dataGridViewCellStyle1;
+            for (int i = 0; i < rows.Length + 1; i++)
+                dataGridViewSolutions[0, i].Style = dataGridViewCellStyle1;
+            for (int i = 1; i < columns.Length + 1; i++)
+                for (int j = 1; j < rows.Length + 1; j++)
+                    dataGridViewSolutions[i, j].Value = "-";
+
+            for (int i = 1; i < columns.Length + 1; i++)
+                for (int j = 1; j < rows.Length + 1; j++)
+                    if(counts[j - 1, i - 1]!=null)
+                        dataGridViewSolutions[i, j].Value = counts[j-1,i-1];
+
+            for (int i = 1; i < columns.Length + 1; i++)
+                dataGridViewSolutions[i,0].Value = columns[i - 1];
+            for (int j = 1; j < rows.Length + 1; j++)
+                dataGridViewSolutions[0, j].Value = rows[j-1];
+
+                dataGridViewSolutions[0, 0].Value = "";
+            dataGridViewSolutions[0, 0].ReadOnly = true;
+            
+            dataGridViewResult.Rows.Clear();
+            dataGridViewResult.Columns.Clear();
+            dataGridViewResult.Columns.Add("Z","Z");
+            dataGridViewResult.Columns.Add("Atributes", "Свойсва");
+            string atributes = "";
+            for (int i = 0; i < columnsCount; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < rowsCount; j++)
+                    if(counts[j, i]!=null)
+                        sum += (int)counts[j, i];
+                if(sum!=columns[i])
+                {
+                    atributes += "недопустимая\n";
+                    break;
+                }
+            }
+            for (int i = 0; i < rowsCount; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j <columnsCount; j++)
+                    if (counts[ i,j] != null)
+                        sum += (int)counts[i, j];
+                if (sum != rows[i])
+                {
+                    if(!atributes.Contains("недопустимая"))
+                        atributes += "недопустимая";
+                    break;
+                }
+            }
+            if (!atributes.Contains("недопустимая"))
+                atributes += "допустимая";
+            atributes += " \n";
+            if (columns.Length+rows.Length-1==basis.Count)
+                atributes += "базисное";
+            else atributes += "небазисное";
+            atributes += " \n";
+            for (int i = 0; i < basis.Count; i++)
+                if (counts[basis[i].X, basis[i].Y] != null && counts[basis[i].X, basis[i].Y] < 0)
+                    atributes += "вырожденное";
+            if(!atributes.Contains("вырожденное"))
+                atributes += "невырожденное";
+            int initZ = 0;
+            for (int i = 0; i < basis.Count; i++)
+                initZ += (int)counts[basis[i].X, basis[i].Y] * costs[basis[i].X, basis[i].Y];
+            dataGridViewResult.Rows.Add(initZ,atributes);
+        }
+    }
+}
